f49e28389e89ae8842bcd6c7bf76b53d323332d8
Kirill Bertosh <kirillbertosh@gmail.com>
2018-03-01 19:21:44 +0300

    add transactional services for other dao classes

---
+++ server/src/main/java/service/UserService.java
@@ -0,0 +1,76 @@
+package service;
+
+import dao.implementations.UserDao;
+import dbException.DbException;
+import entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.*;
+
+@Service
+@Transactional
+public class UserService {
+    
+    @Autowired
+    private UserDao dao;
+    
+    public User create(User user) throws DbException {
+        try {
+            return dao.create(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new DbException("Exception in creating new user transaction");
+        }
+    }
+    
+    public User update(int id, User updateUser) throws DbException {
+        try {
+            User user = dao.getById(id);
+            user.setCountry(updateUser.getCountry());
+            user.setDescription(updateUser.getDescription());
+            user.setEmail(updateUser.getEmail());
+            user.setFirstName(updateUser.getFirstName());
+            user.setLastName(updateUser.getLastName());
+            user.setPhoneNumber(updateUser.getPhoneNumber());
+            user.setRating(updateUser.getRating());
+            return dao.update(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new DbException("Exception in updating user transaction");
+        }
+    }
+    
+    public boolean delete(int id) throws DbException {
+        try {
+            User user = dao.getById(id);
+            if (user != null) {
+                dao.delete(user);
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new DbException("Exception in deleting user transaction");
+        }
+    }
+    
+    public List getAll() throws DbException {
+        try {
+            return dao.getAll();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new DbException("Exception in creating user list transaction");
+        }
+    }
+    
+    public User getById(int id) throws DbException {
+        try {
+            return dao.getById(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new DbException("Exception in getting user by id transaction");
+        }
+    }
+}
