e627f15341d4b858ae3daa2ef6634868e9e695f4
Kirill Bertosh <kirillbertosh@gmail.com>
2018-03-06 00:06:48 +0300

    add role controller

---
+++ server/src/main/java/bertosh/controller/RoleController.java
@@ -0,0 +1,69 @@
+package bertosh.controller;
+
+import bertosh.dbException.DbException;
+import bertosh.entities.Role;
+import bertosh.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class RoleController {
+
+    @Autowired
+    private RoleService service;
+
+    @GetMapping("/roles")
+    public ResponseEntity getAll() throws DbException {
+        List list = service.getAll();
+        if (list != null) {
+            return new ResponseEntity<>(list, HttpStatus.OK);
+        } else {
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        }
+    }
+
+    @GetMapping("/roles/{id}")
+    public ResponseEntity getById(@PathVariable Long id) throws DbException {
+        Role role = service.getById(id);
+        if (role != null) {
+            return new ResponseEntity<>(role, HttpStatus.OK);
+        } else {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/roles/{id}")
+    public ResponseEntity update(@PathVariable Long id, @RequestBody Role role) throws DbException {
+        role = service.update(id, role);
+        if (role != null) {
+            return new ResponseEntity<>(role, HttpStatus.OK);
+        } else {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/roles")
+    public ResponseEntity<Role> create(@RequestBody Role role) throws DbException {
+        role = service.create(role);
+        return new ResponseEntity<>(role, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/roles/{id}")
+    public ResponseEntity delete(@PathVariable Long id) throws DbException {
+        if (service.delete(id)) {
+            return new ResponseEntity(HttpStatus.OK);
+        } else {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @ExceptionHandler(DbException.class)
+    public ResponseEntity handleDbException(DbException e) {
+        return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
