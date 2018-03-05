ed7efa671e5d0f34090e1cf60c200ddd4261b935
Kirill Bertosh <kirillbertosh@gmail.com>
2018-03-05 22:41:52 +0300

    add tomcat deployment

--- server/src/main/java/bertosh/Application.java
+++ server/src/main/java/bertosh/Application.java
@@ -1,10 +1,28 @@
 package bertosh;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+/*@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+*/
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
